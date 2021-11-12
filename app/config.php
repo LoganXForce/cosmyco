c60eabcbd237261c674590169446162c4d75e5cc
Juan Armando Ferrer Sanchez <juanferrer185@gmail.com>
2021-11-12 18:51:14 +0000

    Update config.php

--- app/config.php
+++ app/config.php
@@ -5,7 +5,7 @@ session_start();
 define('IS_LOCAL' , in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']));
 
 // La URL de nuestro proyecto
-define('URL'      , (IS_LOCAL ? 'https://cotizadorcmc.herokuapp.com/' : ''));
+define('URL'      , (IS_LOCAL ? 'https://gitlab.com/VicMarDzib/cotizador-cos.git' : 'https://cotizadorcmc.herokuapp.com/'));
 
 // Rutas para carpetas
 define('DS'       , DIRECTORY_SEPARATOR);
